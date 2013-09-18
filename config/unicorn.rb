a10912b1c9ba16fcf30ca904d6a390abd9ae9baa
Jenny Fang <jenny.fang@pulseenergy.com>
2013-09-18 09:37:40 -0700

    Adding unicorn.rb to gitignore. Adding border to graphs.

--- config/unicorn.rb
+++
@@ -1,5 +0,0 @@
-listen 5001 # by default Unicorn listens on port 8080
-worker_processes 2 # this should be >= nr_cpus
-pid "/home/crporter/workspace/graphiti/unicorn/unicorn.pid"
-stderr_path "/home/crporter/workspace/graphiti/unicorn/unicorn.log"
-stdout_path "/home/crporter/workspace/graphiti/unicorn/unicorn.log"
